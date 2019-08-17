0c4107b11d9830f8ab087c9bf88bfa5a9f003188
Serpent7776 <serpent7776@gmail.com>
2019-08-17 20:31:09 +0200

    added player sprite

--- src/game.js
+++ src/game.js
@@ -4,42 +4,71 @@ let {init, Sprite, GameLoop} = kontra;
 
 let {canvas, context} = init();
 
-let ground = Sprite({
-	x: 0,
-	y: 0,
-	tile_size: 20,
-	width: 640,
-	height: 480,
-	calc_ground_tile_color: function(x, y) {
-		let sx = k => Math.sin(x * k);
-		let sy = k => Math.cos(y * k);
-		let h = 120 + 25 * (sx(1) * sy(1));
-		let s = 100 - 7 * (sx(1.5) + sy(1.7)) / 2;
-		let l = 45 + 2.5 * (sx(360) + sy(420)) / 2;
-		return {h: h, s: s, l:l};
-	},
-	render: function() {
-		let ts = this.tile_size;
-		for (let y = 0; y < this.width; y += ts) {
-			for (let x = 0; x < this.width; x += ts) {
-				let {h, s, l} = this.calc_ground_tile_color(x, y);
-				this.context.fillStyle = `hsl(${h}, ${s}%, ${l}%)`;
-				this.context.fillRect(x, y, ts, ts);
-				this.context.strokeStyle = '#999';
-				this.context.lineWidth = 1;
-				this.context.strokeRect(x, y, ts, ts);
+kontra.setImagePath('../gfx/');
+
+kontra.load('player.png').then(_ => {
+	let charsSheet = kontra.SpriteSheet({
+		image: kontra.imageAssets.player,
+		frameWidth: 18,
+		frameHeight: 20,
+		animations: {
+			walk: {
+				frames: '24..27',
+				frameRate: 12,
+			},
+		},
+	});
+	let player = kontra.Sprite({
+		x: 100,
+		y: 100,
+		width: 18,
+		height: 20,
+		anchor: {
+			x: 0.5,
+			y: 0.5,
+		},
+		animations: charsSheet.animations,
+	});
+	player.playAnimation('walk');
+	let ground = Sprite({
+		x: 0,
+		y: 0,
+		tile_size: 20,
+		width: 640,
+		height: 480,
+		calc_ground_tile_color: function(x, y) {
+			let sx = k => Math.sin(x * k);
+			let sy = k => Math.cos(y * k);
+			let h = 120 + 25 * (sx(1) * sy(1));
+			let s = 100 - 7 * (sx(1.5) + sy(1.7)) / 2;
+			let l = 45 + 2.5 * (sx(360) + sy(420)) / 2;
+			return {h: h, s: s, l:l};
+		},
+		render: function() {
+			let ts = this.tile_size;
+			for (let y = 0; y < this.width; y += ts) {
+				for (let x = 0; x < this.width; x += ts) {
+					let {h, s, l} = this.calc_ground_tile_color(x, y);
+					this.context.fillStyle = `hsl(${h}, ${s}%, ${l}%)`;
+					this.context.fillRect(x, y, ts, ts);
+					this.context.strokeStyle = '#999';
+					this.context.lineWidth = 1;
+					this.context.strokeRect(x, y, ts, ts);
+				}
 			}
 		}
-	}
-});
+	});
 
-let loop = GameLoop({
-	update: function() {
-		ground.update();
-	},
-	render: function() {
-		ground.render();
-	},
-});
+	let loop = GameLoop({
+		update: function() {
+			ground.update();
+			player.update();
+		},
+		render: function() {
+			ground.render();
+			player.render();
+		},
+	});
 
-loop.start(); 
+	loop.start();
+})
