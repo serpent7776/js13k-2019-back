a7b0c37fecefeae32ae7bd278d434b53fb7a59cc
Serpent7776 <serpent7776@gmail.com>
2019-08-17 22:32:35 +0200

    player is always in centre of the screen

--- src/game.js
+++ src/game.js
@@ -7,6 +7,9 @@ let {canvas, context} = init();
 kontra.setImagePath('../gfx/');
 kontra.initKeys();
 
+let screenWidth = kontra.getCanvas().width;
+let screenHeight = kontra.getCanvas().height;
+
 kontra.load('player.png').then(_ => {
 	let charsSheet = kontra.SpriteSheet({
 		image: kontra.imageAssets.player,
@@ -19,12 +22,16 @@ kontra.load('player.png').then(_ => {
 			},
 		},
 	});
+	let camera = {
+		x: 0,
+		y: 0,
+	};
 	let player = kontra.Sprite({
-		x: 100,
-		y: 100,
+		x: screenWidth / 2,
+		y: screenHeight / 2,
 		width: 18,
 		height: 20,
-		movement_speed: 64,
+		movement_speed: 32,
 		anchor: {
 			x: 0.5,
 			y: 0.5,
@@ -32,16 +39,16 @@ kontra.load('player.png').then(_ => {
 		animations: charsSheet.animations,
 		update: function(dt) {
 			if (kontra.keyPressed('up')) {
-				this.y -= this.movement_speed * dt;
+				camera.y -= this.movement_speed * dt;
 			}
 			if (kontra.keyPressed('down')) {
-				this.y += this.movement_speed * dt;
+				camera.y += this.movement_speed * dt;
 			}
 			if (kontra.keyPressed('left')) {
-				this.x -= this.movement_speed * dt;
+				camera.x -= this.movement_speed * dt;
 			}
 			if (kontra.keyPressed('right')) {
-				this.x += this.movement_speed * dt;
+				camera.x += this.movement_speed * dt;
 			}
 			this.advance(dt);
 		}
@@ -63,14 +70,16 @@ kontra.load('player.png').then(_ => {
 		},
 		render: function() {
 			let ts = this.tile_size;
-			for (let y = 0; y < this.width; y += ts) {
-				for (let x = 0; x < this.width; x += ts) {
+			for (let ty = 0; ty < this.width; ty += ts) {
+				for (let tx = 0; tx < this.width; tx += ts) {
+					let x = tx + camera.x;
+					let y = ty + camera.y;
 					let {h, s, l} = this.calc_ground_tile_color(x, y);
 					this.context.fillStyle = `hsl(${h}, ${s}%, ${l}%)`;
-					this.context.fillRect(x, y, ts, ts);
+					this.context.fillRect(tx, ty, ts, ts);
 					this.context.strokeStyle = '#999';
 					this.context.lineWidth = 1;
-					this.context.strokeRect(x, y, ts, ts);
+					this.context.strokeRect(tx, ty, ts, ts);
 				}
 			}
 		}
